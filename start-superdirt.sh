2044e60198ba98f297e743a3ce357990781f3591
Simon Crowe <simon.r.crowe@protonmail.com>
2022-10-02 16:05:06 +0100

    Add device option to start script

--- start-superdirt.sh
+++ start-superdirt.sh
@@ -1,3 +1,23 @@
-jackd -r -d alsa -r 44100 &
-sclang `dirname "$0"`/start-superdirt.scd
+#! /bin/sh
 
+
+device="hw:0"
+while getopts "d:" opt; do
+	case $opt in
+		d)
+			device=$OPTARG;;
+		\?)
+			echo "Invald option: -$OPTARG" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argment." >&2
+			exit 1
+			;;
+	esac
+done
+
+echo "Starting JACK with device: $device"
+jackd -r -d alsa -r 44100	-d $device &
+
+sclang `dirname "$0"`/start-superdirt.scd
