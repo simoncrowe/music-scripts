8569e16835dec92b25bd82705ff261e49d86861c
Simon Crowe <simon.r.crowe@protonmail.com>
2022-10-09 21:12:15 +0100

    Replace tabs with spaces

--- start-superdirt.sh
+++ start-superdirt.sh
@@ -3,21 +3,21 @@
 
 device="hw:0"
 while getopts "d:" opt; do
-	case $opt in
-		d)
-			device=$OPTARG;;
-		\?)
-			echo "Invald option: -$OPTARG" >&2
-			exit 1
-			;;
-		:)
-			echo "Option -$OPTARG requires an argment." >&2
-			exit 1
-			;;
-	esac
+  case $opt in
+    d)
+      device=$OPTARG;;
+    \?)
+      echo "Invald option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argment." >&2
+      exit 1
+      ;;
+  esac
 done
 
 echo "Starting JACK with device: $device"
-jackd -r -d alsa -r 44100	-d $device &
+jackd -r -d alsa -r 44100  -d $device &
 
 sclang `dirname "$0"`/start-superdirt.scd
